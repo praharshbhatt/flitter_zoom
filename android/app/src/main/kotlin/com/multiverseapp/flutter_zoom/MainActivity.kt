eb91034debd8b7d625a2442f8f06b355e5249eff
praharsh bhatt <prahu.bhatt@gmail.com>
2020-08-25 19:29:09 +0530

    Added flutter zoom example app

---
+++ android/app/src/main/kotlin/com/multiverseapp/flutter_zoom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.multiverseapp.flutter_zoom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom/start_meeting_screen.dart';
+import 'join_screen.dart';
+import 'meeting_screen.dart';
+
+
+void main() => runApp(ExampleApp());
+
+class ExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Example Zoom SDK',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      navigatorObservers: [ ],
+      initialRoute: '/',
+      routes: {
+        '/': (context) => JoinWidget(),
+        '/meeting': (context) => MeetingWidget(),
+        '/startmeeting': (context) => StartMeetingWidget(),
+      },
+    );
+  }
+}
